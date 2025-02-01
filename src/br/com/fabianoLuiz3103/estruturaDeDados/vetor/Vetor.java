bba1ddc27e59ef02d4d830b6935b79dd9c273279
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-01 12:12:25 -0300

    Iniciando classe vetor

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
