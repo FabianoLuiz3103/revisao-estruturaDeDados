41225657b59a31ce68d02ebe9795aeb6756fc04d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-01 13:36:52 -0300

    Retornando o tamanho do vetor e seus elementos

--- src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/Vetor.java
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/Vetor.java
@@ -1,5 +1,7 @@
 package br.com.fabianoLuiz3103.estruturaDeDados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 
     private String[] elementos;
@@ -28,4 +30,31 @@ public class Vetor {
         }
     }
 
+    public int tamanho(){
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        /*
+        Exemplo -> this.elementos = {1, 2}
+        for(i < 2-1) --> Até a penúltima posição com vírgula
+        this.elementos[0] = 1
+        ,
+        if(tamanho > 0){
+        coloca a útima posição sem vírgula depois
+        }
+         */
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i = 0; i < this.tamanho-1; i++){
+            sb.append(this.elementos[i]);
+            sb.append(",");
+        }
+        if(tamanho > 0){
+            sb.append(elementos[tamanho-1]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
