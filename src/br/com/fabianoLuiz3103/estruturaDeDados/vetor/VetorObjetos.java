3d95f09699166fd55882d25d55c12dc4801a92e0
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-03 14:51:58 -0300

    Generalizando o vetor para um vetor de Object

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/VetorObjetos.java
@@ -0,0 +1,91 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.vetor;
+
+public class VetorObjetos {
+
+    private Object[] elementos;
+    private int tamanho;
+
+    public VetorObjetos(int capacidade){
+        this.elementos = new Object[capacidade <= 0 ? 1 : capacidade];
+        this.tamanho = 0;
+    }
+
+    public void adiciona(Object elemento){
+        this.aumentarCapacidade();
+        this.elementos[this.tamanho]=elemento;
+        this.tamanho++;
+    }
+
+    public void adiciona(Object elemento, int posicao) throws IllegalArgumentException{
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida! ");
+        }
+        this.aumentarCapacidade();
+        if(this.elementos[posicao] != null){
+            for(int i = tamanho-1; i >= posicao; i--){
+                this.elementos[i+1] = this.elementos[i];
+            }
+        }
+        this.elementos[posicao]=elemento;
+        this.tamanho++;
+    }
+
+    public Object busca(int posicao) throws IllegalArgumentException{
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida! ");
+        }
+        return this.elementos[posicao];
+
+    }
+
+    public int busca(Object elemento) {
+        for(int i = 0; i < tamanho; i++){
+            if(this.elementos[i].equals(elemento)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void aumentarCapacidade(){
+        if(this.tamanho == this.elementos.length){
+            Object[] elementosNovos = new Object[this.elementos.length*2];
+            for(int i = 0; i < this.elementos.length; i++){
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public void remove(int posicao) throws IllegalArgumentException{
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida! ");
+        }
+        for(int i = posicao; i < this.tamanho-1; i++){
+            this.elementos[i] = this.elementos[i+1];
+        }
+        this.elementos[tamanho-1] = null;
+        this.tamanho--;
+    }
+
+    public int tamanho(){
+        return this.tamanho;
+    }
+
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i = 0; i < this.tamanho-1; i++){
+            sb.append(this.elementos[i]);
+            sb.append(",");
+        }
+        if(tamanho > 0){
+            sb.append(elementos[tamanho-1]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
