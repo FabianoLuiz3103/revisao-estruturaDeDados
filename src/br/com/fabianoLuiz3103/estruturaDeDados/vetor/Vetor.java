ed55e3d7f60cea6456edf4c4ab3f03a5fcf50cf5
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-03 14:08:11 -0300

    Adicionando método para adicionar mais capacidade e remover elemento de qualquer posição

--- src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/Vetor.java
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/vetor/Vetor.java
@@ -7,7 +7,7 @@ public class Vetor {
     private int tamanho;
 
     public Vetor(int capacidade){
-        this.elementos = new String[capacidade];
+        this.elementos = new String[capacidade <= 0 ? 1 : capacidade];
         this.tamanho = 0;
     }
 
@@ -20,26 +20,24 @@ public class Vetor {
         }
     }*/
 
-    public void adiciona(String elemento) throws Exception{
-        if(this.tamanho < this.elementos.length){
+    public void adiciona(String elemento){
+            this.aumentarCapacidade();
             this.elementos[this.tamanho]=elemento;
             this.tamanho++;
-        } else{
-            throw new Exception("\n\t\tErro! O vetor já está cheio!");
-        }
     }
 
     public void adiciona(String elemento, int posicao) throws IllegalArgumentException{
         if(!(posicao >= 0 && posicao < tamanho)){
             throw new IllegalArgumentException("Posição inválida! ");
         }
+        this.aumentarCapacidade();
         if(this.elementos[posicao] != null){
             for(int i = tamanho-1; i >= posicao; i--){
                 this.elementos[i+1] = this.elementos[i];
             }
         }
         this.elementos[posicao]=elemento;
-        tamanho++;
+        this.tamanho++;
     }
 
     public String busca(int posicao) throws IllegalArgumentException{
@@ -62,9 +60,40 @@ public class Vetor {
        return -1;
     }
 
+    //Método para adicionar capacidade
+    private void aumentarCapacidade(){
+       if(this.tamanho == this.elementos.length){
+           String[] elementosNovos = new String[this.elementos.length*2];
+           for(int i = 0; i < this.elementos.length; i++){
+               elementosNovos[i] = this.elementos[i];
+           }
+           this.elementos = elementosNovos;
+       }
+    }
+
+    public void remove(int posicao) throws IllegalArgumentException{
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida! ");
+        }
+        for(int i = posicao; i < this.tamanho-1; i++){
+            this.elementos[i] = this.elementos[i+1];
+        }
+        /*
+        o último elemento do vetor continuará com o mesmo valor, mas o
+        tamanho lógico não, esse último elemento será sobrescrito quando
+        um novo elemento for adicionado, mas eu posso fazer isso
+        this.elementos[tamanho-1] = null
+        tamanho-1 é o novo tamanho lógico, então se for 4
+        [0,3] a posição 4 (por ser a próxima pos "vazia" ) será sobrescrita com um novo valor ao add
+         */
+        this.elementos[tamanho-1] = null;
+        this.tamanho--;
+    }
+
     public int tamanho(){
         return this.tamanho;
     }
+
 
     @Override
     public String toString() {
