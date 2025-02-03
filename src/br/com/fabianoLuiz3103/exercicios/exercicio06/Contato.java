cc3360ffc9fa8899de331cb17b8ed4ff19dbb895
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-03 17:53:52 -0300

    Exercicio 06 - Criando classe vetor e classe contato

---
+++ src/br/com/fabianoLuiz3103/exercicios/exercicio06/Contato.java
@@ -0,0 +1,56 @@
+package br.com.fabianoLuiz3103.exercicios.exercicio06;
+
+public class Contato {
+
+    private static int contador;
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Contato(){
+        this.id = contador++;
+    }
+
+    public Contato(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.id = contador++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n *** CONTATO #" + id);
+        sb.append(" *** ");
+        sb.append("\n- NONE: " + this.nome);
+        sb.append("\n- EMAIL: " + this.email);
+        sb.append("\n- TELEFONE: " + this.telefone);
+        return sb.toString();
+    }
+}
