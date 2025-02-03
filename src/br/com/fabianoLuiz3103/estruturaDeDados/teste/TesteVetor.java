bdd843a9692cb6ecf7c46cfcffc5b062b89ddfcd
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-03 14:54:25 -0300

    Testando primeira implementação de vetor genérico

--- src/br/com/fabianoLuiz3103/estruturaDeDados/teste/TesteVetor.java
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/teste/TesteVetor.java
@@ -1,34 +1,49 @@
 package br.com.fabianoLuiz3103.estruturaDeDados.teste;
 
 import br.com.fabianoLuiz3103.estruturaDeDados.vetor.Vetor;
+import br.com.fabianoLuiz3103.estruturaDeDados.vetor.VetorObjetos;
 
-import java.io.StreamTokenizer;
+import javax.sql.rowset.spi.SyncProvider;
+import java.awt.*;
+
 
 public class TesteVetor {
 
     public static void main(String[] args) {
 
-        Vetor vetor = new Vetor(5);
-        try{
-            vetor.adiciona("1");
-            vetor.adiciona("2");
-            vetor.adiciona("5");
-            vetor.adiciona("8");
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        VetorObjetos vetor = new VetorObjetos(3);
+        vetor.adiciona(3); //Tipo primitivo sendo transformado em objeto
+        /*
+        autoboxing -> O Java vai converter o int automaticamente para a um objeto
+        da classe Integer
 
-        System.out.println(vetor.tamanho());
+        Mas para recuperar esse valor eu preciso de um cast explicito
+        int numero = (Integer) array[1];
+         */
+        vetor.adiciona(4);
+        vetor.adiciona("palavra"); //Posso misturar string com int ou qualquer outro objeto porque herdam de object
 
-       System.out.println(vetor);
+        System.out.println(vetor);
 
-        try{
-            vetor.busca(5);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        Contato c1 = new Contato("contato 1", "telefone 1", "email 1");
+        Contato c2 = new Contato("contato 2", "telefone 2", "email 2");
+        Contato c3 = new Contato("contato 3", "telefone 3", "email 3");
 
-        System.out.println(vetor.busca("8"));
-        System.out.println(vetor.busca(vetor.busca("9")));
+        Contato c4 = new Contato("contato 3", "telefone 3", "email 3");
+
+        VetorObjetos contatos = new VetorObjetos(0);
+        contatos.adiciona(c1);
+        contatos.adiciona(c2);
+        contatos.adiciona(c3);
+        System.out.println(contatos);
+
+        System.out.println(contatos.busca(c4));
+        /*
+        Como eu sobrescrevi o método equals na classe
+        Contato ele vai comparar todos os atributos além da referência
+        de memória, então ele vai ver que c4 é igual a c3 e vai retornar a posição
+        de c3 no vetor
+         */
+
     }
 }
