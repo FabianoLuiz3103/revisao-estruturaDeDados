ac884c81afa8c1c55eda35a28e8a1dd6fb485bef
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 18:50:35 -0300

    Exercicio 03 - Lista03 (fila) - Thread de atendimento

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista03lista/exercicio03/ThreadAtendimento.java
@@ -0,0 +1,27 @@
+package br.com.fabianoLuiz3103.exercicios.lista03lista.exercicio03;
+
+import br.com.fabianoLuiz3103.estruturaDeDados.fila.FilaComPrioridade;
+
+public class ThreadAtendimento implements Runnable{
+
+    private FilaComPrioridade<Senha> fila;
+
+    public ThreadAtendimento(FilaComPrioridade<Senha> fila) {
+        this.fila = fila;
+    }
+
+    @Override
+    public void run() {
+
+        while (!fila.isEmpty()){
+
+            try{
+                System.out.println("\n\t\tSenha atendida:\n"+fila.remove());
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n\t\tATENDIMENTO FINALIZADO!");
+    }
+}
