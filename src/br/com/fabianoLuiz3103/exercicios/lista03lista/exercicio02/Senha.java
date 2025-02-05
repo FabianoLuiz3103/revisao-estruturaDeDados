6b7026065bfea84f97b4a883c91c092f2cfd19c3
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 17:36:51 -0300

    Exercicio 02 - Lista03 (fila) - Criando classe senha

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista03lista/exercicio02/Senha.java
@@ -0,0 +1,38 @@
+package br.com.fabianoLuiz3103.exercicios.lista03lista.exercicio02;
+
+public class Senha{
+
+    private static int contadorPrioridade;
+    private static int contadorNormal;
+
+    private String senha;
+
+    public Senha(boolean prioridade){
+        if(prioridade){
+            this.senha = "P"+(++contadorPrioridade);
+        }else{
+            this.senha = "N"+(++contadorNormal);
+        }
+    }
+
+    public static void setContadorPrioridade(int contadorPrioridade) {
+        Senha.contadorPrioridade = contadorPrioridade;
+    }
+
+    public static void setContadorNormal(int contadorNormal) {
+        Senha.contadorNormal = contadorNormal;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return senha;
+    }
+}
