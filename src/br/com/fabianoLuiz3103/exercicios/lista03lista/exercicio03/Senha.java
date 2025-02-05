60c3b6dcc5fcfff64d5bfcb612284d6d252862c4
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 18:50:11 -0300

    Exercicio 03 - Lista03 (fila) - Classe senha

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista03lista/exercicio03/Senha.java
@@ -0,0 +1,54 @@
+package br.com.fabianoLuiz3103.exercicios.lista03lista.exercicio03;
+
+public class Senha implements Comparable<Senha>{
+
+    private String senha;
+    private int prioridade;
+
+    public Senha(){}
+
+    public Senha(String senha, int prioridade) {
+        this.senha = senha;
+        this.prioridade = prioridade;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n ** SENHA **");
+        sb.append("\n- SENHA: " + senha);
+        sb.append("\n- PRIORIDADE: " + getPrioridade(prioridade));
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Senha s) {
+        if(this.prioridade > s.getPrioridade()){
+            return 1;
+        }else if(this.prioridade < s.getPrioridade()){
+            return -1;
+        }
+        return 0;
+    }
+
+    private String getPrioridade(int prioridade){
+        return switch (prioridade){
+            case 1 -> "verde";
+            case 2 -> "amarelo";
+            case 3 -> "vermelho";
+            default -> "S/P";
+        };
+    }
+}
