29eff18aba8db4c484609778a3155a4fb6948b99
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 19:49:23 -0300

    Exercicio 04 - Lista03 (fila) - Classe jogador

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista03lista/exercicio04/Jogador.java
@@ -0,0 +1,23 @@
+package br.com.fabianoLuiz3103.exercicios.lista03lista.exercicio04;
+
+public class Jogador {
+
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
