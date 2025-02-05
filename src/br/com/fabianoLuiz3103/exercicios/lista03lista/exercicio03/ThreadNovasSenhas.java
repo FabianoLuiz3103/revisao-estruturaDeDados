804050a1808c04d5ae5b0181794b2b3c1624ce1d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 18:50:51 -0300

    Exercicio 03 - Lista03 (fila) - Thread de novas senhas

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista03lista/exercicio03/ThreadNovasSenhas.java
@@ -0,0 +1,32 @@
+package br.com.fabianoLuiz3103.exercicios.lista03lista.exercicio03;
+
+import br.com.fabianoLuiz3103.estruturaDeDados.fila.FilaComPrioridade;
+
+import java.util.Random;
+
+public class ThreadNovasSenhas implements Runnable{
+
+    private FilaComPrioridade<Senha> fila;
+    private int contPacientes = 7;
+    private Random r = new Random();
+
+    public ThreadNovasSenhas(FilaComPrioridade<Senha> fila) {
+        this.fila = fila;
+    }
+
+    @Override
+    public void run() {
+
+        for(int i = 0; i < 8; i++){
+            try {
+                Thread.sleep(4000);
+                Senha s = new Senha(("S"+contPacientes), r.nextInt(3)+1);
+                fila.adiciona(s);
+                contPacientes++;
+                System.out.println("\n\tSenha adicionada na fila: \n"+s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
