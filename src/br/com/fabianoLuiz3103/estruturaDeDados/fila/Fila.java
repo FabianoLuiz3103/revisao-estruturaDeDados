7b243e6bf0f776368b32e650d436255c177632e7
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 12:52:24 -0300

    Começando classe fila

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/fila/Fila.java
@@ -0,0 +1,16 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.fila;
+
+import br.com.fabianoLuiz3103.estruturaDeDados.estruturaEstatica.EstruturaEstatica;
+
+
+public class Fila <T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+}
