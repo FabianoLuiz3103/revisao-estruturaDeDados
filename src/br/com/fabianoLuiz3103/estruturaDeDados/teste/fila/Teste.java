8b9f34255e6c10fcbdec35cd45aa39335e46a649
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-05 12:54:46 -0300

    Teste classe fila

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/teste/fila/Teste.java
@@ -0,0 +1,26 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.teste.fila;
+
+import br.com.fabianoLuiz3103.estruturaDeDados.fila.Fila;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Fila<String> fila = new Fila<>();
+
+        fila.adiciona("A");
+        fila.adiciona("B");
+        fila.adiciona("C");
+        fila.adiciona("D");
+        System.out.println(fila);
+        System.out.println(fila.primeiroDaFila());
+        System.out.println(fila.ultimoDaFila());
+        System.out.println(fila.isEmpty());
+
+        fila.remove();
+        System.out.println(fila);
+        System.out.println(fila.primeiroDaFila());
+        System.out.println(fila.ultimoDaFila());
+
+    }
+}
