82b30836a24ea63611d3c89d6f752ef4966ff61d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-06 15:40:57 -0300

    Revisão Lista encadeada - Primeiros testes com a lista encadeada

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/teste/listaEncadeada/Teste01.java
@@ -0,0 +1,21 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.teste.listaEncadeada;
+
+import br.com.fabianoLuiz3103.estruturaDeDados.listaEncadeada.ListaEncadeada;
+
+public class Teste01 {
+
+    public static void main(String[] args) {
+
+        ListaEncadeada<Integer> listaEncadeada = new ListaEncadeada<>();
+        listaEncadeada.adiciona(1);
+        System.out.println(listaEncadeada);
+        listaEncadeada.adiciona(2);
+        listaEncadeada.adiciona(3);
+        listaEncadeada.adiciona(4);
+        System.out.println(listaEncadeada.getTamanho());
+
+        System.out.println(listaEncadeada);
+
+
+    }
+}
