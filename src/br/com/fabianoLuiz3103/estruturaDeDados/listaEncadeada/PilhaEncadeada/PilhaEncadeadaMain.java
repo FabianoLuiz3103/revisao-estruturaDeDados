fc382990588f2fb4db6fe95d6f9528c4134a7d98
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-10 08:55:14 -0300

    Testando classe pilha

---
+++ src/br/com/fabianoLuiz3103/estruturaDeDados/listaEncadeada/PilhaEncadeada/PilhaEncadeadaMain.java
@@ -0,0 +1,20 @@
+package br.com.fabianoLuiz3103.estruturaDeDados.listaEncadeada.PilhaEncadeada;
+
+public class PilhaEncadeadaMain {
+
+    public static void main(String[] args) {
+
+        PilhaEncadeada<String> pilhaEncadeada = new PilhaEncadeada<>();
+
+        pilhaEncadeada.push("A");
+        pilhaEncadeada.push("B");
+        pilhaEncadeada.push("C");
+
+        System.out.println(pilhaEncadeada);
+        System.out.println(" -- topo: " + pilhaEncadeada.peek());
+
+        while (!pilhaEncadeada.isEmpty()){
+            System.out.println(pilhaEncadeada.pop());
+        }
+    }
+}
